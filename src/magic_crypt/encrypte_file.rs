5aeefec68766521e561d3586e46a4b91cf0113cb
Mateo Lafalce <mateolafalce3@gmail.com>
2023-10-08 08:55:21 +0800

    Update encrypte_file.rs

--- src/magic_crypt/encrypte_file.rs
+++ src/magic_crypt/encrypte_file.rs
@@ -2,9 +2,31 @@
 mod get_password;
 
 use magic_crypt::MagicCryptTrait;
-use std::io::Read;
+use std::io::{Read, Write};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+use std::process;
 
 pub fn encrypte_file(file_path: &str) {
+    println!("You will overwrite {} file.", file_path);
+    println!("Enter to continue");
+    let mut user_input = std::io::stdout().into_raw_mode().unwrap();
+    let input_in_console = std::io::stdin();
+
+    for received_key in input_in_console.keys() {
+        match received_key.unwrap() {
+            Key::Char('\n') => {
+                encrypte(file_path);
+            },
+            _ => {
+                process::exit(1)
+            }
+        }
+        user_input.flush().unwrap();
+    }
+}
+
+pub fn encrypte(file_path: &str) {
+    std::process::Command::new("clear").status().unwrap();
     let password = get_password::get_password("lock");
     match std::fs::File::open(file_path.clone()) {
         Ok(mut file) => {
