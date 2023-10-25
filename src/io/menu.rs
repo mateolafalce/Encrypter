3cb05ef4797f2290e86111df0dbb2054878da23d
Mateo Lafalce <mateolafalce3@gmail.com>
2023-10-25 19:33:50 +0800

    Update menu.rs

--- src/io/menu.rs
+++ src/io/menu.rs
@@ -1,33 +1,27 @@
 #[path = "./show_menu_options.rs"]
 mod show_menu_options;
 
-use std::io::Write;
-use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+use k_board::{Keyboard, Keys};
 
 pub fn menu() -> u8 {
     show_menu_options::show_menu_options("lock selected");
-    let mut user_input = std::io::stdout().into_raw_mode().unwrap();
-    let input_in_console = std::io::stdin();
     let mut encrypt_decrypt_option: u8 = 0;
-
-    for received_key in input_in_console.keys() {
-        match received_key.unwrap() {
-            Key::Up => {
+    for key in Keyboard::new() {
+        match key {
+            Keys::Up => {
                 show_menu_options::show_menu_options("lock selected");
                 encrypt_decrypt_option = 0;
             }
-            Key::Down => {
+            Keys::Down => {
                 show_menu_options::show_menu_options("unlock selected");
                 encrypt_decrypt_option = 1;
             }
-            Key::Char('\n') => {
+            Keys::Enter => {
                 break;
             }
             _ => {}
         }
-        user_input.flush().unwrap();
     }
-
     std::process::Command::new("clear").status().unwrap();
     encrypt_decrypt_option
 }
